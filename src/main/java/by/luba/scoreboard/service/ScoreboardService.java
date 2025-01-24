02ac4f36731a66ac03ca3f0e9affeb71e7308324
AndrewLubanec <lubanec@credentially.io>
2025-01-24 22:08:58 +0100

    added docs for methods startMatch and updateScore

--- src/main/java/by/luba/scoreboard/service/ScoreboardService.java
+++ src/main/java/by/luba/scoreboard/service/ScoreboardService.java
@@ -5,8 +5,26 @@ import java.util.List;
 import by.luba.scoreboard.domain.Match;
 
 public interface ScoreboardService {
+    /**
+     * Start a new match.
+     *
+     * @param homeTeam  Home team name.
+     * @param awayTeam  Away team name.
+     * @return  {@code true} if the match was successfully started, {@code false} otherwise.
+     *          The match is not started if the home/away team names are empty
+     *          or if the match is already in progress.
+     */
     boolean startMatch(String homeTeam, String awayTeam);
 
+    /**
+     * Update the score of the existing match.
+     *
+     * @param homeTeam  Home team name.
+     * @param awayTeam  Away team name.
+     * @param homeScore Home team score.
+     * @param awayScore Away team score.
+     * @return  {@code true} if the match score was successfully updated, {@code false} otherwise.
+     */
     boolean updateScore(String homeTeam,
                      String awayTeam,
                      int homeScore,
